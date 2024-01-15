3278f8b079463d3e8e47cfae2207d69cdfae2030
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-01-15 17:46:15 +0300

    finish logging

--- Ludis.cpp
+++ Ludis.cpp
@@ -6,11 +6,11 @@ using namespace std;
 int main(){
     // variables
     int total_numbers = 100; // total number of accounts
-    int count = 0; // number of accounts that have been created?
-    int logged_in_user = -1; // the index of logged in user
+    int count = 1; // number of accounts that have been created?
+    int logged_in_user = 0; // the index of logged in user
     char transaction;
-    int login;
-    int manager_password = 1234, manager_account = 1001000, manager_balance = 0;
+    int login_acc,login_pin = 0,my_acc,new_acc;
+    int manager_password = 1234, manager_account = 1001000, manager_balance = 0, system_constant = 1001;
 
 
     // array of account
@@ -37,12 +37,32 @@ int main(){
         transaction = toupper(transaction);
         
         if (transaction == 'O'){
-
+            while (login_pin < 100 || login_pin >= 1000){
+                cout<<"Choose a 3 digit pin code that starts without 0 for your account :";
+                cin>>login_pin;
+            }
+            
         }else{
             if (logged_in_user < 0){
-                cout<<"You are not logged in\n"<<"Enter your account number : 1001";
-                cin>>login;
+                cout<<"You are not logged in\n"<<"Enter your account number : "<<system_constant;
+                cin>>login_acc;
+            }else{
+                login_acc = account[logged_in_user][0] - manager_account;
+                cout<<"Your account : "<<account[logged_in_user][0]<<endl;
             }
+            cout<<"Enter your pin :";
+            cin>>login_pin;
+            my_acc = manager_account + login_acc;
+            // logging in
+            for (int i = 0; i < total_numbers; i++){
+                if (account[i][0] == my_acc && account[i][1] == login_pin){
+                    cout<<"Logged in successfully!\n";
+                    cout<<"Your account number : "<<account[i][0]<<endl;
+                }
+            }
+
+            
+            
         }
         
     }
