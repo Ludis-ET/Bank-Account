3ec3a4189bbd00418ae5388a923c3dda5884d868
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-01-15 15:33:16 +0300

    ludis project

--- ludis.cpp
+++ ludis.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 int main(){
+    // variables
+    int total_numbers = 100;
 
+    // array of account
+    int account[total_numbers] = {};
+    string password[total_numbers] = {};
 
     return 0;
 }
