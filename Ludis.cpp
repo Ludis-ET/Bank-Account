7eadb2c571db10755cf5d22a598ff33b2b2abfc9
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-01-15 18:15:56 +0300

    edits for creating account

--- Ludis.cpp
+++ Ludis.cpp
@@ -9,7 +9,7 @@ int main(){
     int count = 1; // number of accounts that have been created?
     int logged_in_user = -1; // the index of logged in user
     char transaction;
-    int login_acc,login_pin = 0,my_acc,new_acc;
+    int login_acc,login_pin = 0,my_acc,new_acc,new_balance = -1;
     int manager_password = 1234, manager_account = 1001000, manager_balance = 0, system_constant = 1001;
 
 
@@ -37,16 +37,24 @@ int main(){
         transaction = toupper(transaction);
         
         if (transaction == 'O'){
-            while (login_pin < 100 || login_pin >= 1000){
-                cout<<"Choose a 3 digit pin code that starts without 0 for your account :";
-                cin>>login_pin;
+            if (count < total_numbers){
+                while (login_pin < 100 || login_pin >= 1000 || new_balance <= 10){
+                    cout<<"Choose a 3 digit pin code that starts without 0 for your account :";
+                    cin>>login_pin;
+                    cout<<"Deposit money (minimum amount - $10) : $";
+                    cin>>new_balance;
+                }
+                new_acc = manager_account + count;
+                cout<<"Your account is created successfully!\n"<<"Your account number is "<<new_acc<<endl;
+                account[count][0] = new_acc;
+                account[count][1] = login_pin;
+                balance[count] = new_balance;
+                logged_in_user = count;
+                count++;
+
+            }else{
+                cout<<"A maximum number of accounts has been reached. You can't create a new account now.\n";
             }
-            new_acc = manager_account + count;
-            cout<<"Your account is created successfully!\n"<<"Your account number is "<<new_acc<<endl;
-            account[count][0] = new_acc;
-            account[count][1] = login_pin;
-            logged_in_user = count;
-            count++;
         }else{
             if (logged_in_user < 0){
                 cout<<"You are not logged in\n"<<"Enter your account number : "<<system_constant;
@@ -58,12 +66,23 @@ int main(){
             cout<<"Enter your pin :";
             cin>>login_pin;
             my_acc = manager_account + login_acc;
+
+
             // logging in
             for (int i = 0; i < total_numbers; i++){
                 if (account[i][0] == my_acc && account[i][1] == login_pin){
                     cout<<"Logged in successfully!\n";
                     cout<<"Your account number : "<<account[i][0]<<endl;
+                    break;
+                }else{
+                    cout<<"Invalid Pin Code!\n";
+                    break;
                 }
+            }
+
+            // transactions
+            if (transaction == 'B'){
+
             }
 
             
