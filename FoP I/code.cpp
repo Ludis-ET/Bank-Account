a763467d7266e5a09f233a789d456388a73a966a
Liben Gebremedhin <gebremedhinliben@gmail.com>
2024-01-22 15:38:04 -0800

    Added some features to the project.

--- FoP I/code.cpp
+++ FoP I/code.cpp
@@ -2,15 +2,6 @@
 #include<cctype>
 using namespace std;
 int main(){
-    // variables
-    int total_numbers = 100; // total number of accounts
-    int count = 1; // number of accounts that have been created?
-    int logged_in_user = -1; // the index of logged in user
-    int manager_password = 123, manager_account = 1001000,  system_constant = 1001;
-    float manager_balance = 1.5;
-    int login_acc,login_pin,my_acc,new_acc,new_balance = -1;
-    char transaction;
-    bool check_user = true,terminate=false;
 
 
     // manager variables
